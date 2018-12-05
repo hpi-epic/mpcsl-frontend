d06ed50e78318e3423096e58006e007fadcd8035
Theresa <theresa.zobel@student.hpi.de>
2018-12-05 15:19:30 +0100

    Try to add onclick event to interface

--- src/components/ListElementDataset/ListElementDataset.tsx
+++ src/components/ListElementDataset/ListElementDataset.tsx
@@ -1,11 +1,12 @@
-import { Badge, Button, Card, Dropdown, List, Menu } from 'antd';
-import { BadgeProps } from 'antd/lib/badge';
+import { Button, Card, List } from 'antd';
+import { AnchorButtonProps } from 'antd/lib/button/button';
 import React from 'react';
 import './ListElementDataset.css';
 
 interface IPropsListElementDataset {
   title: string;
   content: string;
+  onDelete: (e: React.MouseEventHandler<HTMLAnchorElement>) => void;
 }
 
 function ListElementDataset(props: IPropsListElementDataset) {
@@ -19,9 +20,17 @@ function ListElementDataset(props: IPropsListElementDataset) {
   return (
     <div>
       <List.Item>
-        <Card className='Card' title={cardTitle}>
+        <Card
+          className='Card'
+          title={cardTitle}
+        >
           <p className='Card-Content'>{props.content}</p>
-          <Button className='List-Buttons'>Delete</Button>
+          <Button
+            className='List-Buttons'
+            onClick={props.onDelete}
+          >
+          Delete
+          </Button>
           <Button className='List-Buttons'>View</Button>
         </Card>
       </List.Item>
