a87717bec1b9a194f8f31cd1eba26366ab59bcba
Theresa Zobel <theresa.zobel@student.hpi.de>
2018-12-05 16:23:03 +0100

    Create List Component for Dataset (#14)
    * Add list element component for dataset manager
    
    * Try to add onclick event to interface
    
    * Fix click event for viewing and deleting datasets
    
    * Add click props for buttons in experiment list component
    
    * Minor fixes

---
+++ src/components/ListElementExperiment/ListElementExperiment.tsx
@@ -0,0 +1,81 @@
+import { Badge, Button, Card, Dropdown, List, Menu } from 'antd';
+import { BadgeProps } from 'antd/lib/badge';
+import React from 'react';
+import './ListElementExperiment.css';
+
+interface IPropsListElementExperiment {
+  title: string;
+  status: BadgeProps['status'];
+  statusText: string;
+  content: string;
+  onEdit: (e: React.MouseEvent<HTMLElement>) => void;
+  onDuplicate: (e: React.MouseEvent<HTMLElement>) => void;
+  onDelete: (e: React.MouseEvent<HTMLElement>) => void;
+  onRunStart: (e: React.MouseEvent<HTMLElement>) => void;
+  onExplore: (e: React.MouseEvent<HTMLElement>) => void;
+}
+
+function ListElementExperiment(props: IPropsListElementExperiment) {
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <Button
+          className='Dropdown-Button'
+          onClick={props.onEdit}
+        >
+        Edit
+        </Button>
+      </Menu.Item>
+      <Menu.Item>
+        <Button
+          className='Dropdown-Button'
+          onClick={props.onDelete}
+        >
+        Delete
+        </Button>
+      </Menu.Item>
+      <Menu.Item>
+        <Button
+          className='Dropdown-Button'
+          onClick={props.onDuplicate}
+        >
+        Duplicate
+        </Button>
+      </Menu.Item>
+    </Menu>
+  );
+
+  const cardTitle = (
+    <div>
+      <h2 className='Card-Title'>{props.title}</h2>
+      <Badge className='Card-Badge' status={props.status} text={props.statusText} />
+    </div>
+  );
+
+  return (
+    <div>
+      <List.Item>
+        <Card className='Card' title={cardTitle}>
+          <p className='Card-Content'>{props.content}</p>
+          <Dropdown overlay={menu} placement='bottomLeft'>
+            <Button className='List-Buttons' icon='ellipsis' />
+          </Dropdown>
+          <Button
+            className='List-Buttons'
+            onClick={props.onRunStart}
+          >
+          Run
+          </Button>
+          <Button
+            className='List-Buttons'
+            onClick={props.onExplore}
+          >
+          Explore
+          </Button>
+        </Card>
+      </List.Item>
+    </div>
+  );
+}
+
+export default ListElementExperiment;
