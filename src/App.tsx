a51ac02f9f6b00111a218d36de17f4bb02948b58
Galbaxx <victor.kuenstler@outlook.com>
2018-12-12 09:34:10 +0100

    Fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import colors from './constants/colors';
 
 import './App.css';
@@ -12,7 +12,7 @@ class App extends Component {
   public render() {
     return (
       <div style={{ background: colors.contentBackground }}>
-        <Route exact={true} path='/' component={PipelineManager} />
+        <Redirect from="/" to={Routes.projectManager} />
         <Route path={Routes.manager} component={PipelineManager} />
         <Route path={Routes.graphExplorer} component={GraphExplorer} />
         <Route path={Routes.runtimeManager} component={RuntimeManager} />
