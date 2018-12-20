73eeaf264b3d00035928a24f774c8c222170c1d0
Galbaxx <victor.kuenstler@outlook.com>
2018-12-20 15:39:07 +0100

    Rewrite graph renderer to render in a more 'react-way'

--- src/containers/GraphExplorer/GraphRenderer.tsx
+++ src/containers/GraphExplorer/GraphRenderer.tsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions/graphExplorer';
 import { IStoreState } from '../../types';
 import { Dispatch } from 'redux';
-import { D3Graph } from '../../types/graph';
+import { D3Graph, D3GraphLink } from '../../types/graph';
 
 export interface IGraphRendererProps {
   onFetchGraph: () => void;
@@ -17,20 +17,17 @@ export interface IGraphRendererState {
 }
 
 const graphSettings = {
-  nodeRadius: 10
+  nodeRadius: 10,
+  strokeWidth: 1,
+  linkColor: 'black',
+  arrowColor: 'black'
 };
 
 class GraphRenderer extends React.Component<
   IGraphRendererProps,
   IGraphRendererState
 > {
-  force?: d3.Simulation<any, any>;
-  svgElement?: SVGSVGElement | null;
-  svg?: d3.Selection<any, any, any, any> | null;
-  link?: d3.Selection<any, any, any, any>;
-  node?: d3.Selection<any, any, any, any>;
-  lables?: d3.Selection<any, any, any, any>;
-  circles?: d3.Selection<any, any, any, any>;
+  force: d3.Simulation<any, any>;
 
   constructor(props: IGraphRendererProps) {
     super(props);
@@ -38,110 +35,101 @@ class GraphRenderer extends React.Component<
       width: window.innerWidth,
       height: window.innerHeight
     };
-  }
 
-  componentDidMount() {
-    this.setupD3Graph();
-  }
-
-  componentDidUpdate() {
-    this.setupD3Graph();
-  }
-
-  setupD3Graph = () => {
-    if (this.force) {
-      this.force!.restart();
-      this.node!.remove();
-      this.link!.remove();
-    }
     this.force = d3
       .forceSimulation()
-      .nodes(this.props.selectedGraph.nodes)
       .force('charge', d3.forceManyBody().strength(-130))
-      .force(
-        'link',
-        d3
-          .forceLink(this.props.selectedGraph.links)
-          .distance(50)
-          .id(function(d: any) {
-            return d.id;
-          })
-      )
       .force(
         'center',
         d3.forceCenter(this.state.width / 2, this.state.height / 2)
       )
       .force('collision', d3.forceCollide().radius(15));
+  }
 
-    this.svg = d3
-      .select(this.svgElement!)
-      .append('svg')
-      .attr('width', this.state.width)
-      .attr('height', this.state.height);
-
-    this.svg
-      .append('defs')
-      .append('marker')
-      .attr('id', 'arrow')
-      .attr('viewBox', '0 -5 10 10')
-      .attr('refX', 20)
-      .attr('markerWidth', 10)
-      .attr('markerHeight', 10)
-      .attr('orient', 'auto')
-      .append('svg:path')
-      .attr('d', 'M0,-5L10,0L0,5');
-
-    this.link = this.svg
-      .append('g')
-      .attr('class', 'links')
-      .selectAll('line')
-      .data(this.props.selectedGraph.links)
-      .enter()
-      .append<SVGLineElement>('line')
-      .attr('stroke', '#999999')
-      .attr('marker-end', 'url(#arrow)');
-
-    this.node = this.svg
-      .append('g')
-      .attr('class', 'nodes')
-      .selectAll('g')
-      .data(this.props.selectedGraph.nodes)
-      .enter()
-      .append('g');
-
-    this.circles = this.node
-      .append<SVGCircleElement>('circle')
-      .attr('r', graphSettings.nodeRadius);
-
-    this.lables = this.node
-      .append('text')
-      .text(d => d.id)
-      .attr('x', graphSettings.nodeRadius)
-      .attr('y', 0);
-
-    this.node
-      .append('title')
-      .text(d => d.id);
-
-    this.force!.on('tick', () => {
-      this.link!.attr('x1', (d: any) => d.source.x)
-        .attr('y1', (d: any) => d.source.y)
-        .attr('x2', (d: any) => d.target.x)
-        .attr('y2', (d: any) => d.target.y);
-
-      this.node!
-        .attr('transform', (d: any) => `translate(${d.x},${d.y})`);
+  componentDidMount() {
+    this.force.on('tick', () => {
+      this.forceUpdate();
     });
-  };
+  }
+
+  componentWillReceiveProps(nextProps: IGraphRendererProps) {
+    this.force.nodes(nextProps.selectedGraph.nodes).force(
+      'link',
+      d3
+        .forceLink(nextProps.selectedGraph.links)
+        .distance(50)
+        .id(function(d: any) {
+          return d.id;
+        })
+    );
+    this.force.restart();
+  }
+
+  shouldComponentUpdate(
+    nextProps: IGraphRendererProps,
+    nextState: IGraphRendererState
+  ) {
+    if (nextProps.selectedGraph) {
+      console.log(nextProps);
+      return true;
+    }
+    return false;
+  }
 
   render() {
-    this.setupD3Graph();
+    const defs = (
+      <defs>
+        <marker
+          id="arrow"
+          viewBox="-0 -5 10 10"
+          refX={graphSettings.nodeRadius + 6}
+          refY="0"
+          orient="auto"
+          markerWidth="13"
+          markerHeight="13"
+          //xoverflow="visible"
+        >
+          <path d="M 0,-5 L 10 ,0 L 0,5" fill={graphSettings.arrowColor} style={{stroke: 'none'}} />
+        </marker>
+      </defs>
+    );
+    const nodes = this.props.selectedGraph.nodes.map(node => {
+      const transform = `translate(${node.x},${node.y})`;
+      return (
+        <g className="node" key={node.id} transform={transform}>
+          <circle r={graphSettings.nodeRadius} />
+          <text x={10} y={0}>
+            {node.id}
+          </text>
+        </g>
+      );
+    });
+
+    const links = this.props.selectedGraph.links.map(
+      (link: D3GraphLink, id: number) => (
+        <line
+          className="link"
+          key={id}
+          strokeWidth={graphSettings.strokeWidth}
+          stroke={graphSettings.linkColor}
+          // @ts-ignore
+          x1={link.source.x}
+          // @ts-ignore
+          x2={link.target.x}
+          // @ts-ignore
+          y1={link.source.y}
+          // @ts-ignore
+          y2={link.target.y}
+          markerEnd="url(#arrow)"
+        />
+      )
+    );
     return (
-      <svg
-        ref={node => (this.svgElement = node)}
-        width={this.state.width}
-        height={this.state.height}
-      />
+      <svg width={this.state.width} height={this.state.height}>
+        {defs}
+        {links}
+        {nodes}
+      </svg>
     );
   }
 }
