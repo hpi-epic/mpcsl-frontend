f262f8182a2bfa791443032e1087b9b94497bd40
Galbaxx <victor.kuenstler@outlook.com>
2018-12-28 16:53:31 +0100

    Add list of focused nodes

--- src/containers/GraphExplorer/GraphSelection.tsx
+++ src/containers/GraphExplorer/GraphSelection.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import GraphRenderer from './GraphRenderer';
+import { Layout } from 'antd';
+import GraphNodeList from '../../components/GraphNodeList';
+import { connect } from 'react-redux';
+import { IStoreState } from '../../types';
+import './GraphSelection.css';
 
-class GraphSelection extends React.Component {
-  render() {
-    return (
-      <div>
-        <GraphRenderer />
-      </div>
-    );
-  }
+
+interface IPropsGraphSelection {
+  nodes: string[]
 }
 
-export default GraphSelection;
+function GraphSelection(props: IPropsGraphSelection) {
+  return (
+    <Layout>
+      <Layout.Sider className="graphSelectionSider">
+        <GraphNodeList nodes={props.nodes} />
+      </Layout.Sider>
+      <GraphRenderer />
+    </Layout>
+  );
+}
+
+export function mapStateToProps({ nodes }: IStoreState) {
+  return {
+    nodes
+  };
+}
+
+export default connect(mapStateToProps)(GraphSelection);
