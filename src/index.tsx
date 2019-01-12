2b70a41f22c9361420379b650882af8c30a24978
Galbaxx <victor.kuenstler@outlook.com>
2019-01-12 11:39:08 +0100

    Refactor for testing and add snapshot testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+
 import store from './store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
 
