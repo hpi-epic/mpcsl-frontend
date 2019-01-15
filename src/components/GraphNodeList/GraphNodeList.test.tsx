a9fd9ce221f83140e486fe4289eee057cb4906bb
Galbaxx <victor.kuenstler@outlook.com>
2019-01-15 15:21:51 +0100

    Update test

--- src/components/GraphNodeList/GraphNodeList.test.tsx
+++ src/components/GraphNodeList/GraphNodeList.test.tsx
@@ -5,7 +5,9 @@ import GraphNodeList from './GraphNodeList';
 describe('<GraphNodeList />', () => {
   it('renders correctly', () => {
     const nodeList = ['node1', 'node2', 'node3'];
-    const tree = renderer.create(<GraphNodeList nodes={nodeList} />).toJSON();
+    const tree = renderer
+      .create(<GraphNodeList nodes={nodeList} onRemoveNode={() => undefined} />)
+      .toJSON();
 
     expect(tree).toMatchSnapshot();
   });
