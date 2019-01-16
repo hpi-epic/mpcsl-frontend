73dbf29994078e6b5fc8f8ea5c2ad6bb3350e02e
Theresa <theresa.zobel@student.hpi.de>
2019-01-16 15:03:58 +0100

    Add basic graph to exploration, remove search bar

--- src/containers/GraphExplorer/GraphAnnotate/GraphAnnotate.tsx
+++ src/containers/GraphExplorer/GraphAnnotate/GraphAnnotate.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
 
-class GraphAnnotate extends React.Component {
+import GraphRenderer from '../GraphRenderer/GraphRenderer';
+import GraphNodeList from '../../../components/GraphNodeList/GraphNodeList';
+import { Layout } from 'antd';
+
+import { ID3GraphNode } from '../../../types/graphTypes';
+
+interface IGraphExplorationProps {
+  nodes: ID3GraphNode[];
+}
+
+class GraphAnnotate extends React.Component<
+{},
+IGraphExplorationProps
+> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      nodes: [ {id: 1, label: 'a'} ],
+    };
+  }
+
   public render() {
-    return <div>GraphAnnotate</div>;
+    return(
+    <div>
+      <Layout>
+          <Layout.Sider className='graphSelectionSider'>
+            <GraphNodeList nodes={this.state.nodes} />
+          </Layout.Sider>
+        <GraphRenderer />
+      </Layout>
+    </div>
+    );
   }
 }
 
