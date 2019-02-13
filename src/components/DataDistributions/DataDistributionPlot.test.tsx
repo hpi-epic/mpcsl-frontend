5c11dd5c19abc0724132a260e08fc7e1781ae7ab
Galbaxx <victor.kuenstler@outlook.com>
2019-02-13 11:14:02 +0100

    Add tests

---
+++ src/components/DataDistributions/DataDistributionPlot.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import DataDistributionPlot from './DataDistributionPlot';
+import { IAPIDistributionCategorical } from '../../types';
+
+describe('<DataDistributinoPlot />', () => {
+  it('works for categorical data', () => {
+    const categoricalAPIDataDistribution = {
+      categorical: true,
+      bins: {
+        1: 10,
+      },
+    };
+    const tree = renderer
+      .create(
+        <DataDistributionPlot
+          plotHeight={100}
+          plotWidth={100}
+          data={
+            (categoricalAPIDataDistribution as unknown) as IAPIDistributionCategorical
+          }
+        />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('works for continous data', () => {
+    const categoricalAPIDataDistribution = {
+      categorical: false,
+      bin_edges: [1, 2, 3, 4],
+      bins: [10, 2, 5, 5],
+    };
+    const tree = renderer
+      .create(
+        <DataDistributionPlot
+          plotHeight={100}
+          plotWidth={100}
+          data={
+            (categoricalAPIDataDistribution as unknown) as IAPIDistributionCategorical
+          }
+        />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
