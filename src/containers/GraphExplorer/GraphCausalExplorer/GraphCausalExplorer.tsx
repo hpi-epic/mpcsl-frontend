7312f0f425882e3d0811bd4957d3414324c9be43
Galbaxx <victor.kuenstler@outlook.com>
2019-03-02 17:41:59 +0100

    Add basic tiles

--- src/containers/GraphExplorer/GraphCausalExplorer/GraphCausalExplorer.tsx
+++ src/containers/GraphExplorer/GraphCausalExplorer/GraphCausalExplorer.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import { Mosaic } from 'react-mosaic-component';
+
+import 'react-mosaic-component/react-mosaic-component.css';
+import GraphRenderer from '../GraphRenderer/GraphRenderer';
+
+import './GraphCausalExplorer.css';
+
+const ELEMENT_MAP: { [viewId: string]: JSX.Element } = {
+  a: <div>Left Window</div>,
+  b: <GraphRenderer key='test' isSelectionMode={false} />,
+  c: <div>Bottom Left Window</div>,
+  d: <div>Bottom Right Window</div>,
+};
 
 class GraphCausalExplorer extends React.Component {
   public render() {
-    return <div>GraphCausalExplorer</div>;
+    return (
+      <div style={{ width: '100%', height: '100%', margin: 0 }}>
+        <Mosaic<string>
+          renderTile={(id) => ELEMENT_MAP[id]}
+          initialValue={{
+            direction: 'row',
+            first: 'a',
+            second: {
+              direction: 'column',
+              first: 'b',
+              second: {
+                direction: 'row',
+                first: 'c',
+                second: 'd',
+              },
+              splitPercentage: 70,
+            },
+            splitPercentage: 25,
+          }}
+        />
+      </div>
+    );
   }
 }
 
